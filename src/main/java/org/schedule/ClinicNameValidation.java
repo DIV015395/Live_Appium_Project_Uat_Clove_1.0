ea9693cadf5ebf677a07ead4308ef07f96e629b6
ManjeetSharma <sharmaboymanjeet12@gmail.com>
2023-07-10 15:17:08 +0530

    commited 15:16 10-07-2023 (excel)

--- src/main/java/org/schedule/ClinicNameValidation.java
+++ src/main/java/org/schedule/ClinicNameValidation.java
@@ -1,12 +1,15 @@
 package org.schedule;
 
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
-import org.testng.Assert;
-
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
 
 public class ClinicNameValidation extends AppointmentBooking
 {
+    Set<String> uniqueStrings = new TreeSet<>();
+
     public ClinicNameValidation(AndroidDriver driver)
     {
         super(driver);
@@ -18,13 +21,27 @@ public class ClinicNameValidation extends AppointmentBooking
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         layout_clinicSelect.click();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-//        driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text(\"Wakad\"));"));
         System.out.println(tv_itemTitle.size());
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-
         for (int i = 0; i < tv_itemTitle.size(); i++)
         {
             System.out.println(tv_itemTitle.get(i).getText());
+            uniqueStrings.add(tv_itemTitle.get(i).getText());
+        }
+        driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text(\"Wakad\"));"));
+        for (int i = 0; i < tv_itemTitle.size(); i++)
+        {
+            System.out.println(tv_itemTitle.get(i).getText());
+            uniqueStrings.add(tv_itemTitle.get(i).getText());
+        }
+
+        System.out.println("____________________________________________________");
+        System.out.println("++++++++++++++++All Clinic String++++++++++++++++++++");
+        System.out.println(uniqueStrings.size());
+
+        for (String str : uniqueStrings)
+        {
+            System.out.println(str);
         }
     }
 }
