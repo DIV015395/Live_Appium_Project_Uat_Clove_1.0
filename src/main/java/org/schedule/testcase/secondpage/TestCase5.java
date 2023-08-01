762686e4d65e4306d8d769cd42c30a2b4c5c8537
ManjeetSharma <sharmaboymanjeet12@gmail.com>
2023-08-01 14:53:45 +0530

    MalwareAcception Handling

--- src/main/java/org/schedule/testcase/secondpage/TestCase5.java
+++ src/main/java/org/schedule/testcase/secondpage/TestCase5.java
@@ -1,4 +1,5 @@
 //check mobile number field validation in Appointment page
+//Enter more than 10 digit no.
 
 package org.schedule.testcase.secondpage;
 
@@ -8,6 +9,7 @@ import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import org.openqa.selenium.WebElement;
 import org.schedule.AppointmentPage;
+import org.testng.Assert;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,6 +19,8 @@ public class TestCase5 extends AppointmentPage {
     public AndroidElement massages;
     @AndroidFindBy(id = "com.clove.clover.uat:id/et_contact")
     public WebElement et_contact;
+    @AndroidFindBy(id = "com.clove.clover.uat:id/et_name")
+    public WebElement et_name;
     String getmassage;
 
     public TestCase5(AndroidDriver driver) {
@@ -28,7 +32,7 @@ public class TestCase5 extends AppointmentPage {
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
-    public void scrolling() {
+    public void scrollingDown() {
         driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text(\"SAVE\"));"));
     }
 
@@ -38,10 +42,19 @@ public class TestCase5 extends AppointmentPage {
         iv_save.click();
     }
 
-    public void mobileNumberElevenDigit() {
-        et_contact.sendKeys("70111314981");
+    public void patientName() {
+        et_name.sendKeys("Manjeet Sharma");
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
+    public void scrollingUp() {
+        driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).flingBackward();"));
+    }
+
+    public void toastMassageValidation() {
+        getmassage = massages.getText();
+        System.out.println(getmassage);
+        Assert.assertEquals(getmassage, "Enter 10 digit mobile no!");
+    }
 
 }
--- src/test/java/org/scheduletest/testcases/secondpage/TestCase5Test.java
+++ src/test/java/org/scheduletest/testcases/secondpage/TestCase5Test.java
@@ -6,6 +6,8 @@ import org.desiredcapabilities.DesireCap;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.schedule.SchedulePage;
 import org.schedule.testcase.secondpage.TestCase5;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -18,10 +20,17 @@ public class TestCase5Test {
     public AndroidDriver driver;
 
     @BeforeClass
-    public void driverLaunch() throws MalformedURLException {
-        DesiredCapabilities caps = DesireCap.desire();
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    public void driverLaunch() {
+        Logger logger = LoggerFactory.getLogger(getClass());
+        try {
+            DesiredCapabilities caps = DesireCap.desire();
+            driver = new AndroidDriver(new URL("http://17.0.0.1:4723/wd/hub"), caps);
+            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        } catch (MalformedURLException e) {
+            System.out.println("URL is not Valid");
+            logger.error("Error: Malformed URL - {}", e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     @Test(priority = 1)
@@ -46,8 +55,21 @@ public class TestCase5Test {
     public void appointmentTestCase() {
         TestCase5 obj1 = new TestCase5(driver);
         obj1.mobileNumberNineDigit();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         obj1.scrolling();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         obj1.saveend();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        obj1.scrollingUp();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        obj1.patientName();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        obj1.scrolling();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        obj1.saveend();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        obj1.toastMassageValidation();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
     @AfterClass
     public void driverClose() {
